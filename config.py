a34842a9d93344147bbd3321fcec443e77fed6af
Inojj <bchernysh@gmail.com>
2024-11-06 12:21:49 +0300

    доработки и readme

---
+++ config.py
@@ -0,0 +1,2 @@
+RANEPA_SCHEDULE_URL_PAGE = 'https://spb.ranepa.ru/raspisanie/mn-3-23-01-02-noyabr/'
+EXPECTED_GROUPS = ('МН-3-23-02', 'МН-3-23-01/02')
--- main.py
+++ main.py
@@ -1,46 +1,51 @@
-import arrow
 import requests
 import datetime
 
 from bs4 import BeautifulSoup
 from ics import Calendar, Event
+import zoneinfo
 
-url = 'https://spb.ranepa.ru/raspisanie/mn-3-23-01-02-noyabr/'
+from config import EXPECTED_GROUPS, RANEPA_SCHEDULE_URL_PAGE
+
+moscow_tz = zoneinfo.ZoneInfo("Europe/Moscow")
 year = int(datetime.date.today().year)
 
 calendar = Calendar()
 
-response = requests.get(url, verify=False)
+response = requests.get(RANEPA_SCHEDULE_URL_PAGE, verify=False)
 soup = BeautifulSoup(response.text, 'lxml')
 
 rasp_table = soup.find(class_='row-hover')
 days = rasp_table.find_all('tr')
 for day in days:
-    time = day.find(class_='column-4').text.split('-')
-    time_begin = time[0].split('.')
-    hour_begin = time_begin[0]
-    minutes_begin = time_begin[-1]
-    time_end = time[-1].split('.')
-    hour_end = time_end[0]
-    minutes_end = time_end[-1]
-    event = Event()
-    event.name = f"{day.find(class_='column-7').text.strip()} {day.find(class_='column-5').text} {day.find(class_='column-6').text}"
-    # arrow.Arrow(year, 5, 5, 12, 30, 45)
-    event.begin = datetime.datetime(year=year,
-                                    month=int(day.find(class_='column-3').text),
-                                    day=int(day.find(class_='column-2').text),
-                                    hour=int(hour_begin),
-                                    minute=int(minutes_begin),
-                                    )
-    event.end = datetime.datetime(year=year,
-                                  month=int(day.find(class_='column-3').text),
-                                  day=int(day.find(class_='column-2').text),
-                                  hour=int(hour_end),
-                                  minute=int(minutes_end),
-                                  )
-    event.description = f"Преподаватель: {day.find(class_='column-9').text}. Занятие проходит: {day.find(class_='column-10').text}"
-    calendar.events.add(event)
+    group = day.find(class_='column-5').text
+    if group in EXPECTED_GROUPS:
+        time = day.find(class_='column-4').text.split('-')
+        time_begin = time[0].split('.')
+        hour_begin = int(time_begin[0])
+        minutes_begin = int(time_begin[-1])
+        time_end = time[-1].split('.')
+        hour_end = int(time_end[0])
+        minutes_end = int(time_end[-1])
+        event = Event(name=f"{day.find(class_='column-7').text.strip()} {group} {day.find(class_='column-6').text}",
+                      begin=datetime.datetime(year=year,
+                                              month=int(day.find(class_='column-3').text),
+                                              day=int(day.find(class_='column-2').text),
+                                              hour=hour_begin,
+                                              minute=minutes_begin,
+                                              tzinfo=moscow_tz,
+                                              ),
+                      end=datetime.datetime(year=year,
+                                            month=int(day.find(class_='column-3').text),
+                                            day=int(day.find(class_='column-2').text),
+                                            hour=hour_end,
+                                            minute=minutes_end,
+                                            tzinfo=moscow_tz,
+                                            ),
+                      description=f"Преподаватель: {day.find(class_='column-9').text}. "
+                                  f"Занятие проходит: {day.find(class_='column-10').text}"
+                      )
+        calendar.events.add(event)
 
-
-with open('my.ics', 'w', encoding='utf') as my_file:
+with open('ranepa_schedules.ics', 'w', encoding='utf') as my_file:
     my_file.writelines(calendar.serialize_iter())
